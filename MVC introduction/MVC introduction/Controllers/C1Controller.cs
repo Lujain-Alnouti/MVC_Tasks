32734d33c218add7c25a093893527055d807ccc8
Lujain-Alnouti <115452443+Lujain-Alnouti@users.noreply.github.com>
2023-01-31 18:40:44 +0300

    Task1

---
+++ MVC introduction/MVC introduction/Controllers/C1Controller.cs
@@ -0,0 +1,48 @@
+﻿using MVC_introduction.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_introduction.Controllers
+{
+    public class C1Controller : Controller
+    {
+        // GET: C1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Sum()
+        {
+            int x = 9 + 9 + 9 + 9 + 9 + 9 + 9;
+            return "<h3>Action 2 Task1 Part2</h3>The Sum = " + x + "<br/> <a href='https://localhost:44306/C1/Information'>Back </a>";
+        }
+
+        public string Information()
+        {
+            
+            return "<h3>Action 1 Task1 Part2</h3>My Name Is Lujain Alnouti , 22 Years Ago " +
+                "<br/> <a href='https://localhost:44306/C1/Sum'> Action 2 </a>"+
+                "<br/> <a href='https://localhost:44306/C1/XXX'> Action 3</a>" +
+                "<br/> <a href='https://localhost:44306/C1/YYY'> Action 4</a>" +
+                "<br/> <a href='https://localhost:44306/Default/showImage'> Task 1 Part 1  4</a>";
+        }
+
+        public string XXX()
+        {
+            
+            return "<h3>Action 3 Task1 Part2</h3> XXX" + "<br/> <a href='https://localhost:44306/C1/Information'>Back </a>";
+        }
+
+        public string YYY()
+        {
+           
+            return "<h3>Action 4 Task1 Part2</h3> YYY" + "<br/> <a href='https://localhost:44306/C1/Information'>Back </a>";
+        }
+
+
+    }
+}
---
+++ MVC introduction/MVC introduction/Controllers/DefaultController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_introduction.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string showImage()
+        {
+            return ("<a href='..\\Images\\Juc.png' download> <img src='..\\Images\\Juc.png'/></a> " +
+                "<br/> <a href='https://localhost:44306/Default/AboutUS'> About Us</a><br/> <a href='https://localhost:44306/Default/Contact'> Contact</a>" +
+                "<br/> <a href='https://localhost:44306/C1/Information'> Task1 Part 2</a>");
+        }
+        //public ActionResult DownLoadImage()
+        //{
+        //    var imagePath = Path.Combine(Server.MapPath("~/Images"), "Juc.png");
+        //    return File(imagePath, "image/jpeg", "Juc.png");
+        //    //string path = Server.MapPath("~/Images/Juc.png");
+        //    //byte[] imageByteData = System.IO.File.ReadAllBytes(path);
+        //    //return File(imageByteData, "image/png");
+
+        //}
+
+        public string AboutUS()
+        {
+            return "<h3>About Us Action</h3><h1> Hello,Welcome In About Us Page</h1> <p>This is task 1 in MVC tutorial</p><br> <a href='https://localhost:44306/Default/showImage'> Back</a>";
+        }
+
+        public string Contact()
+        {
+            return "<h3>Contact Action</h3><h1> Hello, Welcome In Contact Page</h1> <p>Phone Number is : 0780798564</p><br> <a href='https://localhost:44306/Default/showImage'> Back</a>";
+        }
+    }
+}
