7b96f67f67e4c5a32ce0f4ed93bd3d233fa84e19
Lujain-Alnouti <115452443+Lujain-Alnouti@users.noreply.github.com>
2023-02-01 16:27:59 +0300

    Task D2

---
+++ MVCD2/MVCD2/Controllers/Task2Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCD2.Controllers
+{
+    public class Task2Controller : Controller
+    {
+        // GET: Task2
+        List<Models.Student> cor = new List<Models.Student>()
+        {
+            new Models.Student() { id=2,name="HTML",major="HTML",Faculity="XXX" },
+            new Models.Student() { id=3,name="C#",major="C#",Faculity="YYY" },
+            new Models.Student() { id=4,name="CSS",major="CSS",Faculity="OOO" },
+            new Models.Student() { id=5,name="ASP.net",major="ASP.NEt",Faculity="NNN0" },
+            };
+        public ActionResult Student()
+        {
+
+            Session["cor"] = "Lujain Alnouti";
+            //ViewBag.Personal = cor;
+            return View(cor);
+        }
+       
+    }
+}
---
+++ MVCD2/MVCD2/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCD2.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string major { get; set; }
+        public string Faculity { get; set; }
+    }
+}
